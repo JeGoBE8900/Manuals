a1c528060fa0182758e0bf0090182f8ff7dd3e8a
JeGoBE8900 <168869412+JeGoBE8900@users.noreply.github.com>
2026-06-05 11:03:09 +0200

    Update and rename cmd.txt to WindowsOptiesJGO.ps1

---
+++ Other/WindowsOptiesJGO.ps1
@@ -0,0 +1,7 @@
+# reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+# classic right mouse click
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null; Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value ""
+
+#show file extensions
+Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -value 0
